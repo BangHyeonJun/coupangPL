f8bfbe2cd71044804ce66460290d9cea5c9f2e31
BangHyeonJun <bhj2849@naver.com>
2021-01-02 16:41:19 +0900

    오픈 그래프 추가

--- pages/redirect.tsx
+++ pages/redirect.tsx
@@ -38,6 +38,47 @@ function redirect() {
                     name="viewport"
                     content="width=device-width, initial-scale=1, shrink-to-fit=no"
                 />
+
+                {/* 오픈 그래프 */}
+                <meta
+                    name="title"
+                    property="og:title"
+                    content="쿠팡 파트너스 | 리디렉션 생성기"
+                />
+                <meta
+                    name="description"
+                    property="og:description"
+                    content="쿠팡 파트너스로 인한 개인 블로그의 저품질을 막기 위한 리디렉션 생성기 입니다. 기존의 다른 블로그를 통한 번거로운 리디렉션 단계를 최소화 하고 사용자의 수익 보장을 위해 제작되어졌으며, 어떠한 블로그라도 사용이 가능합니다."
+                />
+                <meta property="og:type" content="website" />
+                <meta property="og:locale" content="ko_KR" />
+                <meta
+                    property="og:site_name"
+                    content="Online Coupang Redirection Generator"
+                />
+                <meta
+                    name="image"
+                    property="og:image"
+                    content="/images/opengraph.jpg"
+                />
+                <meta
+                    name="url"
+                    property="og:url"
+                    content="https://cpredirection.com/"
+                />
+
+                {/* 트위터 */}
+                <meta name="twitter:card" content="summary" />
+                <meta
+                    name="twitter:title"
+                    content="쿠팡 파트너스 | 리디렉션 생성기"
+                />
+                <meta
+                    name="twitter:description"
+                    content="쿠팡 파트너스로 인한 개인 블로그의 저품질을 막기 위한 리디렉션 생성기 입니다. 기존의 다른 블로그를 통한 번거로운 리디렉션 단계를 최소화 하고 사용자의 수익 보장을 위해 제작되어졌으며, 어떠한 블로그라도 사용이 가능합니다."
+                />
+                <meta name="twitter:image" content="/images/opengraph.jpg" />
+
                 <script
                     data-ad-client="ca-pub-4278000043835062"
                     async
