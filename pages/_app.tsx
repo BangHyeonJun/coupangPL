64214c9e4d1dd68d4599a9a4f0049cdfaee3c4b7
BangHyeonJun <bhj2849@naver.com>
2021-03-13 22:57:53 +0900

    필요없는 소스 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,19 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { AppProps } from 'next/app';
-import initGA from '../src/utils/ga/index';
-import Router from 'next/router';
+// import initGA from '../src/utils/ga/index';
+// import Router from 'next/router';
 
 // 글로벌 스타일
 import '@/styles/global.scss';
 
 function App({ Component, pageProps }: AppProps) {
-    useEffect(() => {
-        initGA(process.env.NEXT_PUBLIC_GA_ID, Router);
-
-        return () => {
-            initGA(process.env.NEXT_PUBLIC_GA_ID, Router);
-        };
-    });
+    // useEffect(() => {
+    //     initGA(process.env.NEXT_PUBLIC_GA_ID, Router);
+    //
+    //     return () => {
+    //         initGA(process.env.NEXT_PUBLIC_GA_ID, Router);
+    //     };
+    // });
 
     return (
         <>
