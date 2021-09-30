c75af0428d915c3397a3b9bb4a08d9e0607a7bb4
BangHyeonJun <bhj2849@naver.com>
2021-10-01 03:23:17 +0900

    서비스 종료

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,7 +17,13 @@ function App({ Component, pageProps }: AppProps) {
 
     return (
         <>
-            <Component {...pageProps} />
+            <div style={{ display: "flex", flexDirection: "column", alignItems:"center", justifyContent: "center"}}>
+                <h1>쿠팡 파트너스 리디렉션 생성기를 이용해 주셔서 감사합니다.</h1>
+                <h2>더 좋은 서비스로 찾아뵙겠습니다.</h2>
+            </div>
+
+            {/* 서비스 종료로 인한 주석 */}
+            {/*<Component {...pageProps} />*/}
         </>
     );
 }
